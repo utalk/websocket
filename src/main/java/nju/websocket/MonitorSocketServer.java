f46a1b9a64b693fc0a158b93cd32f527c738eaf5
kiki <798838812@qq.com>
2019-03-25 15:46:51 +0800

    modify monitor

--- src/main/java/nju/websocket/MonitorSocketServer.java
+++ src/main/java/nju/websocket/MonitorSocketServer.java
@@ -3,20 +3,23 @@ package nju.websocket;
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
+import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.Set;
 
 /**
  * nio socket服务端
  */
 public class MonitorSocketServer {
+    private final DatagramSocket socket;
+    private final Thread serverThread;
+
     //解码buffer
     private Charset cs = Charset.forName("UTF-8");
     //接受数据缓冲区
@@ -25,6 +28,32 @@ public class MonitorSocketServer {
     private static ByteBuffer rBuffer = ByteBuffer.allocate(1024);
     //选择器（叫监听器更准确些吧应该）
     private static Selector selector;
+
+    public MonitorSocketServer() throws SocketException {
+        this.socket = new DatagramSocket(8888);
+        this.serverThread = new Thread(() -> {
+            try {
+                byte[] receiveData = new byte[1024];
+                while(true) {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    socket.receive(receivePacket);
+                    String msg = new String(receiveData, 0, receivePacket.getLength());
+                    System.out.println(msg);
+
+                    // TODO 这边需要解析一下
+                    JSONObject result = new JSONObject();
+                    result.put("source", "");
+                    result.put("target", "");
+                    result.put("type", "PING");
+
+                    MonitorWebSocket.sendInfo(result.toJSONString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        serverThread.start();
+    }
 
     /**
      * 启动socket服务，开启监听
